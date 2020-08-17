e6c5df468cca72e245833382b9fa441b008b02df
Jeff-Adler <Jeffm.adler@gmail.com>
2020-08-17 16:02:29 -0400

    select drop-down

---
+++ src/index.js
@@ -0,0 +1,27 @@
+QUIZ_URL = "http://localhost:3000/quizzes/"
+NEW_PATH = "new"
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    // Responsible for getting the list of categories from API and rendering them in the HTML
+    const getCategoryList = () => {
+        fetch(QUIZ_URL + NEW_PATH)
+        .then(response => response.json)
+        .then(categories => renderCategories(categories))
+    }
+
+    const renderCategories = (categories) => {
+        const select = document.createElement("select")
+        select.className = "quiz_selection"
+        for (const category in categories) {
+            const option = document.createElement("option")
+            option.value = category["name"]
+            option.innerText = category["name"]
+            select.append(option)
+        }
+        document.querySelector("quiz_create").append(select)
+    }
+
+
+    getCategoryList()
+})
