9fdafa617bba3cee894e124c32d97ea62ba46ccc
sonyavera <sonyavgould@gmail.com>
2020-08-18 12:01:29 -0400

    Error with ApplicationRecord

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 QUIZ_URL = "http://localhost:3000/quizzes/"
 NEW_PATH = "new"
+FIND_PATH = "find"
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -13,6 +14,8 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const renderCategories = (categories) => {
+        const label = document.createElement("label")
+        label.innerText = "Choose a quiz topic:"
         const select = document.createElement("select")
         select.className = "quiz_selection"
         for (const category of categories) {
@@ -21,8 +24,46 @@ document.addEventListener("DOMContentLoaded", () => {
             option.innerText = category["name"]
             select.append(option)
         }
-        document.querySelector("div.quiz_create").append(select)
+        const quizForm = document.querySelector("#quiz-selections")
+        quizForm.append(label, select)
+        submitButton = document.createElement("input")
+        submitButton.type = "submit"
+        submitButton.value = "Submit"
+        quizForm.append(submitButton)
+        
+        quizForm.addEventListener("submit", processForm)
+
     }
+
+    const processForm = (e) => {
+        e.preventDefault()
+        const formElements = e.target.children
+        const difficultyInput = formElements[1].value
+        const quizTopicInput = formElements[3].value
+
+        findQuiz(difficultyInput, quizTopicInput)
+    }
+
+    const findQuiz = (difficultyInput, topicInput) => {
+        const quizObj = {
+            difficulty: difficultyInput,
+            topic: topicInput
+        }
+        const option = {
+        method: "POST",
+        headers: {
+            "content-type": "application/json",
+            "accept": "application/json"
+        },
+        body: JSON.stringify(quizObj) 
+        }
+
+        fetch(QUIZ_URL + FIND_PATH, option)
+        .then(response => response.json())
+        .then(resp => console.log(resp))
+    }
+
+
 
     getCategoryList()
 })
